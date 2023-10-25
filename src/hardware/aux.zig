1eb1eaee4542a899f721b58f10a86e88ff042f78
Eric Rowley <Eric.Rowley@amd.com>
2023-10-25 10:12:12 -0500

    AUX peripheral for MU

--- src/hardware/aux.zig
+++ src/hardware/aux.zig
@@ -1,25 +1,128 @@
-pub const AUX_ENABLE = register(0x215004);
-pub const AUX_MU_IO = register(0x215040);
-pub const AUX_MU_IER = register(0x215044);
-pub const AUX_MU_IIR = register(0x215048);
-pub const AUX_MU_LCR = register(0x21504C);
-pub const AUX_MU_MCR = register(0x215050);
-pub const AUX_MU_LSR = register(0x215054);
-pub const AUX_MU_MSR = register(0x215058);
-pub const AUX_MU_SCRATCH = register(0x21505C);
-pub const AUX_MU_CNTL = register(0x215060);
-pub const AUX_MU_STAT = register(0x215064);
-pub const AUX_MU_BAUD = register(0x215068);
+const std = @import("std");
+const c = @import("constants.zig");
 
-pub fn uartInit() void {
-    AUX_ENABLE.* |= 1;
-    AUX_MU_CNTL.* = 0;
-    AUX_MU_LCR.* = 3;
-    AUX_MU_IER.* = 0;
-    AUX_MU_IIR.* = 0xC6;
-    AUX_MU_BAUD.* = 270;
-    var reg = GPFSEL1.*;
-    reg &= ~@as(@TypeOf(reg), (7 << 12) | (7 << 15));
-    reg |= (2 << 12) | (2 << 15);
-    GPFSEL1.* = reg;
-}
+pub const aux: *volatile AUX = @ptrFromInt(c.mmio_base + 0x215000);
+
+pub const AUX = extern struct {
+    const Self = *volatile @This();
+
+    irq: u32,
+    enables: u32,
+    rsvd0: [14]u32,
+    mu_io: u32,
+    mu_ier: u32,
+    mu_iir: u32,
+    mu_lcr: u32,
+    mu_mcr: u32,
+    mu_lsr: u32,
+    mu_msr: u32,
+    mu_scratch: u32,
+    mu_cntl: u32,
+    mu_stat: u32,
+    mu_baud: u32,
+    rsvd1: [5]u32,
+    spi: [2]SPIRegSet,
+
+    comptime {std.debug.assert(@sizeOf(AUX) == 0x100);}
+
+    pub fn muEnable(self: Self) void {
+        self.enables |= 1;
+    }
+
+    pub fn muDisable(self: Self) void {
+        self.enables &= ~@as(u32, 1);
+    }
+
+    pub fn interruptStatus(self: Self) InterruptStatus {
+        return @bitCast(self.irq);
+    }
+
+    pub fn muClearRecvFIFO(self: Self) void {
+        self.mu_iir = 0x2;
+    }
+
+    pub fn muClearSendFIFO(self: Self) void {
+        self.mu_iir = 0x4;
+    }
+
+    pub fn muClearFIFOs(self: Self) void {
+        self.mu_iir = 0x6;
+    }
+
+    pub fn muSetDataSize(self: Self, size: MUDataSize) void {
+        if (size == .mu7bit)
+            self.mu_lcr &= ~@as(u32, 3)
+        else
+            self.mu_lcr |= 3;
+    }
+
+    pub fn muDataStatus(self: Self) MUDataStatus {
+        return @bitCast(self.mu_lsr);
+    }
+
+    pub fn muDisableComm(self: Self) void {
+        self.mu_cntl &= ~@as(u32, 3);
+    }
+
+    pub fn muEnableComm(self: Self) void {
+        self.mu_cntl |= 3;
+    }
+
+    pub fn muStatus(self: Self) MUStatus {
+        return @bitCast(self.mu_stat);
+    }
+
+    pub fn muSendByte(self: Self, data: u8) void {
+        while (!self.muDataStatus().transmitter_empty) {}
+        self.mu_io = data;
+    }
+};
+
+pub const SPIRegSet = extern struct {
+    cntl0: u32,
+    cntl1: u32,
+    stat: u32,
+    rsvd0: u32,
+    io: u32,
+    peek: u32,
+    rsvd1: [10]u32,
+};
+
+pub const InterruptStatus = packed struct(u32) {
+    mu: bool,
+    spi1: bool,
+    spi2: bool,
+    rsvd: u29,
+};
+
+pub const MUDataSize = enum {
+    mu7bit,
+    mu8bit,
+};
+
+pub const MUDataStatus = packed struct(u32) {
+    data_ready: bool,
+    receiver_overrun: bool,
+    rsvd: u3,
+    transmitter_empty: bool,
+    transmitter_idle: bool,
+    pad: u25,
+};
+
+pub const MUStatus = packed struct(u32) {
+    symbol_available: bool,
+    space_available: bool,
+    receiver_idle: bool,
+    transmitter_idle: bool,
+    receiver_overrun: bool,
+    transmit_full: bool,
+    rts_status: bool,
+    cts_status: bool,
+    transmit_empty: bool,
+    transmitter_done: bool,
+    rsvd0: u6,
+    receive_fill: u4,
+    rsvd1: u4,
+    transmit_fill: u4,
+    rsvd2: u4,
+};
--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,22 @@
 const gpio = @import("hardware/gpio.zig").gpio;
+const aux  = @import("hardware/aux.zig").aux;
 
 export fn main() noreturn {
+    const message = "Hello world!";
     gpio.fnSel(14, .alt5);
     gpio.fnSel(15, .alt5);
     gpio.pud(14, .disable);
     gpio.pud(15, .disable);
+    aux.muEnable();
+    aux.muDisableComm();
+    aux.muSetDataSize(.mu8bit);
+    aux.muClearFIFOs();
+    aux.mu_baud = 270;
+    aux.muEnableComm();
+
+    for (message) |c| {
+        aux.muSendByte(c);
+    }
+
     while (true) {}
 }
